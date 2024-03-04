f35677339e06b3764e5958cf4d5fc90b16bbcc64
ARKIT KARMOKAR <113276950+Arkit-karmokar@users.noreply.github.com>
2024-03-04 13:09:18 +0530

    final commit

---
+++ android/app/src/main/kotlin/com/example/memehub/FlutterActivity.kt
@@ -0,0 +1,5 @@
+package com.example.memehub
+
+open class FlutterActivity {
+
+}
--- android/app/src/main/kotlin/com/example/memehub/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/memehub/MainActivity.kt
@@ -1,5 +1,3 @@
 package com.example.memehub
 
-import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity: FlutterActivity()
